9a85ce8f970fcbbf67b832cf8cfc3403476ad12e
hiroki0331 <hiroki.nakai0331@gmail.com>
2024-12-15 01:15:25 +0900

    add publishStatus enum class and fix Book model and create book sql

---
+++ src/main/kotlin/com/example/book/manager/domain/enum/PublishStatus.kt
@@ -0,0 +1,3 @@
+package com.example.book.manager.domain.enum
+
+enum class PublishStatus { UNPUBLISHED, PUBLISHED }
--- src/main/kotlin/com/example/book/manager/domain/model/Book.kt
+++ src/main/kotlin/com/example/book/manager/domain/model/Book.kt
@@ -1,10 +1,12 @@
 package com.example.book.manager.domain.model
 
-import java.time.LocalDate
+import com.example.book.manager.domain.enum.PublishStatus
+import java.math.BigDecimal
 
 data class Book(
     val id: Long,
     val title: String,
+    val price: BigDecimal,
     val author: String,
-    val publishedDate: LocalDate
+    val status: PublishStatus
 )
